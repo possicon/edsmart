5973c5535e4fa428a7bcb832ebaa94037ccfde77
Onuorah Paschal <onuorahpaschal@gmail.com>
2017-01-03 02:52:00 +0100

    Updates added

--- js/style.js
+++ js/style.js
@@ -1,50 +1,79 @@
 $(document).ready(function() {
   $('#tabs').find('h4').click(function() {
+
     $('#tabs').find('.tab').removeClass('active');
     $(this).parent().addClass('active');
+
   });
 
   $('form.form-submit').submit(function() {
   	var email = $.trim($("input[name='email']").val());;
-  	var password = $.trim($("input[name='password']").val());;
-  	var e_error = $('#emailerror');
-  	var p_error = $('#passworderror')
+  	    password = $.trim($("input[name='password']").val());;
+  	    e_error = $('#emailerror');
+  	    p_error = $('#passworderror')
+
   	if (email === "") {
+
   		e_error.html("Please type your email address");
   		return false;
+
   	} 
   	else {
+
   		e_error.hide();
+
   	};
 
   	if (password === "") {
+
   		p_error.html("Please type your password");
   		return false;
+
   	}
 
   	else {
+
   		p_error.hide();
+
   	};
 
     return true;
 
   });
 
-  $('div.post').hide();
-  $('.read span:last-child').hide();
+  $('div.post, .read span:last-child').hide();
   $('p.read').click(function() {
+
     $(this).siblings('div.post').slideToggle('400');
     $(this).children('span:first-child, span:last-child').toggle();
+
   });
 
   $("#spelling").typed({
+
     strings: [
               "smart <span class='fa fa-times' style='color:#0099FF'></span>",
               "Smart <span class='fa fa-check' style='color:#0099FF'></span>"
               ],
     typeSpeed: 400,
     backDelay: 2000,
-    loop:true
+    loop: true
+
+  });
+
+  $(window).scroll(function () {
+
+    var windowScrollTop = $(window).scrollTop();
+        navBar = $("nav#press-nav");
+
+    if (windowScrollTop <= 1270) {
+      navBar.stop().animate({
+
+        marginTop: windowScrollTop
+
+      }, 300);
+      
+    };
   });
 
 	// var trigger = $('.hamburger'),
